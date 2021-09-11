8d25371d1f3a3ef52c15469f3bf66a08ec0dee23
nhad1702 <anhducnguyenhuu2@gmail.com>
2021-09-11 21:27:08 +0700

    Btap js

---
+++ hw/ls2/app.js
@@ -0,0 +1,62 @@
+// console.log('Hello C4E!'); //console log
+
+// Math.random(); //methods (Math: library)
+
+//6->23: data types
+// let a = 4;
+// let b = 8;
+// let c = 1516;
+// let d = 23.42;
+
+// let sing = 'Who are you?';
+// let doub = "Who am I?";
+
+// let latetoSchool = true;
+
+// let x = null;
+
+// let person = {
+//     fisrtname: "Anh",
+//     lastname: "Duc",
+//     age: "16",
+//     school: "Trung Vuong High School",
+// }
+
+//exercise
+// console.log('JavaScript');
+// console.log(2020);
+// console.log("Woohoo! I love to code.");
+// console.log(4.4);
+
+//Arithmetic Operations
+
+//String Interpolation
+// let age = 7;
+
+// console.log('Tommy is ' + age + ' years old.'); //Noi chuoi
+// console.log(`Tommy is ${age} years old.`); //Noi suy chuoi (??)
+
+//Variables
+// const currency = `$`;
+// let UserIncome = 85000;
+
+// console.log(currency + UserIncome + ' is more than the average income.');
+
+//examples
+// let Name = 'Tommy';
+// const found = 'false';
+// var age1 = 3;
+
+// console.log(Name, found, age1);
+
+//String Concatenation
+// let service = 'credit card';
+// let month = 'May 30th';
+// let display = 'Your ' + service + ' bill is due to ' + month + '.';
+// console.log(display);
+
+//Tenplate Literals
+// let fullname = "Nguyen Huu Anh Duc";
+// console.log(`Hello, ${fullname}`);
+
+// console.log(`Duc is ${12 + 4} years old.`);
