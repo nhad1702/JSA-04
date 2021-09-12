9a4534b9734cb72f40a945877ffb4ee6c8c22d0f
nhad1702 <anhducnguyenhuu2@gmail.com>
2021-09-12 11:10:54 +0700

    luaaaa

---
+++ study/lesson2/app.js
@@ -0,0 +1,39 @@
+// console.log("Hello JSA-04")
+
+// let fullName = "Nguyen Huu Anh Duc";
+// let age = 16;
+// let gender = true;
+
+// let fullName = "Nguyen Huu Anh Duc";
+// let age = 16;
+// let result ;
+// console.log(result);
+
+// function calcAge(age) {
+//     let days = age * 365;
+//     return days;
+// }
+
+// console.log(calcAge(65));
+// console.log(calcAge(0));
+// console.log(calcAge(20));
+
+
+// function remainder(c, d) {
+//     let div = c % d;
+//     return div;
+// }
+
+// console.log(remainder(1, 3));
+// console.log(remainder(-9, 45));
+// console.log(remainder(5, 5));
+
+
+function findPerimeter(e, f) {
+    let peri = (e + f) * 2;
+    return peri;
+}
+
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
