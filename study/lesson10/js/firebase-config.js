299e18c97ba68d14ef5bd41a97a2203bf4bd8eca
nhad1702 <anhducnguyenhuu2@gmail.com>
2021-12-11 22:29:43 +0700

    sua lai

---
+++ study/lesson10/js/firebase-config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyDTi2UXB69A8jMq5pcrKRaGl-xDd7-h5Nw",
+    authDomain: "jsa-04.firebaseapp.com",
+    projectId: "jsa-04",
+    storageBucket: "jsa-04.appspot.com",
+    messagingSenderId: "899756607789",
+    appId: "1:899756607789:web:ec5502a5c9459251376ba4",
+};
+initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
